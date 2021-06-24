6a067ff00d4e603aca7d9942d3b801db9549aa3c
Ilchenko Yaroslav <ilchenckoyaroslav@gmail.com>
2021-06-24 15:21:46 +0300

    completed 8 tasks

---
+++ src/js/task-08.js
@@ -0,0 +1,42 @@
+// const inputEl = document.querySelector('input');
+// console.log(inputEl);
+// const renderBtn = document.querySelector('button[data-action="render"]');
+// console.log(renderBtn);
+// const destroyBtn = document.querySelector('button[data-action="destroy"]');
+// console.log(destroyBtn);
+
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-action=render]");
+const destroyBtn = document.querySelector("button[data-action=destroy]");
+const container = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+    container.classList.add("boxes");
+    amount = Number(input.value);
+    let startBoxSize = 30;
+    if (input.value !== '0' && input.value !== '' && container.children.length !== amount) {
+        do {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = startBoxSize + "px";
+    box.style.height = startBoxSize + "px";
+    box.style.backgroundColor = `${randonRGB()}`;
+    container.appendChild(box);
+    startBoxSize += 10;
+  } while (container.children.length < amount);
+    }
+}
+
+function destroyBoxes() {
+    container.innerHTML = "";
+    input.value = ""
+  // [...container.children].forEach(box => box.remove());
+}
+
+const randomColor = () => Math.floor(Math.random() * 256);
+const randonRGB = () =>
+  `rgb(${randomColor()},${randomColor()},${randomColor()})`;
